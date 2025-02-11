b881c6b8407a67463f9e74711d2a73c41fa57b25
staillebois <sylvain.taillebois@gmail.com>
2025-02-11 22:56:06 +0100

    Share project

---
+++ src/main/kotlin/com/st/GreetingResource.kt
@@ -0,0 +1,15 @@
+package com.st
+
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+
+@Path("/hello")
+class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    fun hello() = "Hello from Quarkus REST"
+    
+}
